926887a22c2de6a0b8e1bd75a31293772333ba65
FENG Wen Hao <150085208@stu.vtc.edu.hk>
2017-11-19 14:24:40 +0800

    new styles

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
-const ko = require('knockout');
-const $ = require('jquery');
+var ko = require('knockout');
+var $ = require('jquery');
 require('jquery.sidr.min');
-const styles = require('./mapstyle');
-const map;
+require('bootstrap.min');
+var styles = require('./mapstyle');
+var map;
 window.styles = styles; //pass it to window object
 
 
 window.initMap = function() {
+	var latlng = new google.maps.LatLng(22.3964,114.1095);
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 40.7413549, lng: -73.9980244 },
-        zoom: 13,
+        center: latlng,
+        zoom: 12,
         styles:window.styles,
         mapControlType: false
     });
 };
+
+
---
+++ js/places.js
@@ -0,0 +1,70 @@
+var places = {
+    id: "4b7e4c3ff964a520b6e82fe3",
+    name: "Tai Mo Shan (大帽山)",
+    contact: {},
+    location: {
+        address: "Centre of the New Territories, Hong Kong",
+        lat: 22.40721006128253,
+        lng: 114.10836963065078,
+        labeledLatLngs: [{
+            label: "display",
+            lat: 22.40721006128253,
+            lng: 114.10836963065078
+        }],
+        distance: 1208,
+        cc: "HK",
+        city: "Tai Mo Shan",
+        country: "Hong Kong",
+        formattedAddress: [
+            "Centre of the New Territories, Hong Kong",
+            "Hong Kong"
+        ]
+    },
+    categories: [{
+        id: "4eb1d4d54b900d56c88a45fc",
+        name: "Mountain",
+        pluralName: "Mountains",
+        shortName: "Mountain",
+        icon: {
+            prefix: "https://ss3.4sqi.net/img/categories_v2/parks_outdoors/mountain_",
+            suffix: ".png"
+        },
+        primary: true
+    }],
+    verified: false,
+    stats: {
+        checkinsCount: 667,
+        usersCount: 437,
+        tipCount: 7
+    },
+    rating: 9,
+    ratingColor: "00B551",
+    ratingSignals: 45,
+    beenHere: {
+        count: 0,
+        marked: false,
+        lastCheckinExpiredAt: 0
+    },
+    hours: {
+        status: "Likely open",
+        richStatus: {
+            entities: [],
+            text: "Likely open"
+        },
+        isOpen: true,
+        isLocalHoliday: false
+    },
+    photos: {
+        count: 0,
+        groups: []
+    },
+    hereNow: {
+        count: 0,
+        summary: "Nobody here",
+        groups: []
+    }
+}
+
+var venues = [
+	
+]
